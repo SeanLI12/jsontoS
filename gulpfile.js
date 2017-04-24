ba914217a8ed9f060cc696e448f41e34bd4b932e
Sean <larcenciel12@gmail.com>
2017-04-24 17:56:12 +0800

    v1

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,104 @@
-var gulp = require('gulp');
-var webserver = require('gulp-webserver');
+var gulp = require('gulp'),
+    jsonToSass = require('gulp-json-to-sass'),
+    gulpSass  = require('gulp-sass'),
+    partnername="Theme_Black",
+    targetname="lp_theader",
+    watch = require('gulp-watch'),
+    assemblenamejson="json/component/"+targetname+".json",
+    assemblenamescss="../Scss/"+partnername+"/Theme_Color_Setting/_"+targetname+".scss",
+    assemblepartnerfolder="../Theme/"+partnername+"/Css",
+    jf = require('jsonfile'),
+    tgScss="../Scss/Theme/"+partnername+"/Css/*.scss",
+    connection,
+    fs = require('fs'),
+    gulpCompass    = require('gulp-compass');
 
-var jsonToSass = require('gulp-json-to-sass');
+
+//引入http websocket
+var http = require('http');
+var ws = require('websocket').server;
+
+// 建立server 並監聽Port 9999
+var PORT = 9999;
+var server = http.createServer().listen(PORT)
+
+
+webSocketServer = new ws({
+    httpServer: server
+});
+
+
+webSocketServer.on('request', function(request) {
+    connection = request.accept('echo-protocol', request.origin);
+
+   
+    connection.on('message', function(message) {
+        console.log(message);
+        
+        
+        var recobj=JSON.parse(message.utf8Data);
+        if(recobj.type=="request"){
+          var getjsonpath="json/component/"+ recobj.compID+".json";
+            partnername=recobj.themeID;
+            targetname=recobj.compID;
+            assemblenamejson="json/component/"+targetname+".json";
+            assemblenamescss="../Scss/"+partnername+"/Theme_Color_Setting/_"+targetname+".scss";
+            assemblepartnerfolder="../Theme/"+partnername+"/Css";
+            tgScss="../Scss/Theme/"+partnername+"/Css/*.scss";
+            jf.readFile(getjsonpath, function(err, obj) {
+             
+              connection.send(JSON.stringify(obj));
+            });
+          
+        }
+        if(recobj.type=="update"){
+          var getjsonpath="json/component/"+ recobj.compID+".json";
+              partnername=recobj.themeID;
+              targetname=recobj.compID;
+              assemblenamejson="json/component/"+targetname+".json";
+              assemblenamescss="../Scss/"+partnername+"/Theme_Color_Setting/_"+targetname+".scss";
+              assemblepartnerfolder="../Theme/"+partnername+"/Css";
+              tgScss="../Scss/Theme/"+partnername+"/Css/*.scss";
+              fs.writeFile(getjsonpath, JSON.stringify(recobj.obj), function(err) {
+                  if (err) 
+                    throw 'error writing file: ' + err;
+                  else
+                    console.log('Write operation complete.');
+                    gulp.run('sasss');
+              });
+ 
+          
+        }
+        
+        
+    });
+
+    
+    connection.on('close', function(reasonCode, description) {
+        console.log('Close');
+    });
+});
+
+
+gulp.task('watchjsonFolder', function () {
+  gulp.watch('json/component/*.json', function() {
+    gulp.run('sasss');
+    console.log("123");
+  });
+  
+})
 
 
 gulp.task('sasss', function () {
-    return gulp.src('scss/*.scss')
-        .pipe(jsonToSass({
-            jsonPath: 'json/vars.json',
-            scssPath: 'sass/_variables.scss'
-        }))
-       
-        .pipe(gulp.dest('dist/css'));
+  return gulp.src(tgScss)
+      .pipe(jsonToSass({
+          jsonPath: assemblenamejson,
+          scssPath: assemblenamescss
+      }))
+      .pipe(gulpSass(),console.log("test"))
+      .pipe(gulp.dest(assemblepartnerfolder));
 });
 
 
 
-gulp.task('default',['sasss']);
+gulp.task('default',['sasss','watchjsonFolder']);
